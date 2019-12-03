fa17c4a22d5adb8e5bc452983ee774fe521fbd3a
Prajwal Saokar <prajwalsaokar@gmail.com>
2019-12-02 23:55:51 -0500

    final program

--- Loan Prediction/finalloanpredictor.py
+++ Loan Prediction/finalloanpredictor.py
@@ -69,22 +69,3 @@ import joblib
 joblib.dump(classifier, 'estimator.pkl', compress=0)
 with open("estimator.pkl", "wb") as model: Pickle.dump(classifier, model)
 with open("estimator.pkl", "rb") as model: thenewmodel = Pickle.load(model)
-#%% Plotting the results
-
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_pred
-X1, X2 = np.meshgrid(np.arange(start = ID.min() - 1, stop =ID.max() + 1, step = 0.01),
-                     np.arange(start = ID.min() - 1, stop = ID.max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Logistic Regression (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
